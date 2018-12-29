e2c767d400eb41bada55d2b37611fd7b297229d0
werliton <werlitoncarlos@gmail.com>
2018-12-29 11:20:35 -0300

    Repair path of the files

---
+++ src/components/Home/Home.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Home.css'
+
+const Home = ({}) => (
+    <div className="Home">
+        <h1>Welcome to Codejobs</h1>
+        <p>
+        In this recipe you will learn how to add styles to
+        components. If you want to learn more you can visit
+        our Youtube Channel at
+        <a href="http://youtube.com/codejobs">Codejobs</a>.
+        </p>
+    </div>
+)
+
+export default Home
