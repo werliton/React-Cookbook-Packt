e960520cb89838bc9ecdfddb5ce24c83953fa11a
werliton <werlitoncarlos@gmail.com>
2018-12-29 12:06:31 -0300

    Using variable state local

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,27 +1,31 @@
 import React from 'react'
 import './Home.css'
 
-const Home = (props) => (
-    <div className="Home">
-        <h1>Welcome to Codejobs</h1>
-        <p>
-            In this recipe you will learn how to add styles to
-            components. If you want to learn more you can visit
-            our Youtube Channel at
-            <a href="http://youtube.com/codejobs">Codejobs</a>.
-        </p>
-        <p>
-            <button
-            style={{
-                backgroundColor: 'gray',
-                border: '1px solid black'
-            }}
-            >
-            Click me!
-            </button>
-        </p>
-        
-    </div>
-)
+class Home extends React.Component{
+    constructor(){
+        super()
+        this.state = {
+            name: ' Carlos'
+        }
+    }
+
+    _updateName(){
+        setTimeout(()=> {
+            this.setState({
+                name:'João Miguel'
+            })
+        }, 10000)
+    }
+
+    render(){
+
+        this._updateName()
+        return (
+            <div className="Home">
+                <p> Hi my name is {this.state.name}</p>
+            </div>
+        )
+    }
+}
 
 export default Home
