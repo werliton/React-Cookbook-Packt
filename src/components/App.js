eeab8b9be57e158e3b0a24fa0a8e70a5933b5d66
werliton <werlitoncarlos@gmail.com>
2018-12-30 18:53:27 -0300

    Styling coins

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import Content from './Home/Content';
-import Footer from '../shared/component/layout/Footer';
-import Timer from './Pomodoro/Timer';
+import Footer from '../shared/component/layout/Footer'; 
 import Coins from './CryptoCoin/Coins';
 
 class App extends Component {
