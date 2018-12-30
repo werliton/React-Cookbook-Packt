1b41fa0a2f2d84d50bfba8affa55c23dcf707aad
werliton <werlitoncarlos@gmail.com>
2018-12-30 00:53:28 -0300

    Implementing challenge book: add buttons play, pause and reset

--- src/components/Pomodoro/Timer.js
+++ src/components/Pomodoro/Timer.js
@@ -9,7 +9,8 @@ export default class Timer extends Component{
                 type:'',
                 message:''
             },
-            time: 0
+            time: 0,
+            btnplayDisabled: true
         }
 
         this.times = {
@@ -98,40 +99,95 @@ export default class Timer extends Component{
         return `${m < 10 ? '0': ''}${m}:${s < 10 ? '0' :''}${s}`
     }
 
+    playTime = (type) => {
+        this.setState({
+            btnplayDisabled:true,
+            alert:{
+                type,
+                message: 'To Play again'
+            }
+        })
+        // Executa a função countDown a cada segundo
+        this.interval = setInterval(this.countDown, 1000)       
+    }
+
+    plauseTime = (type) => {
+        this.setState({
+            btnplayDisabled:false,
+            alert:{
+                type,
+                message: 'It Paused'
+            }
+        })
+        clearInterval(this.interval)
+    }
+
+    resetTime = (type, message) => {
+        const typeFinal = type === 'work'? 'defaultTime':type
+        this.setState({
+            alert:{
+                type: typeFinal,
+                message
+            },
+            time: this.times[typeFinal]
+        })
+    }
+
     render() {
         const { alert: { message, type }, time} = this.state
 
-      return (
-        <div className="Pomodoro">
-            <div className={`alert ${type}`}>
-                {message}
-            </div>
+        return (
+            <div className="Pomodoro">
+                <div className={`alert ${type}`}>
+                    {message}
+                </div>
 
-            <div className="timer">
-                {this.displayTimer(time)}
-            </div>
-            <div className="types">
-                <button
-                    className="start"
-                    onClick={this.setTimeForWork}
-                >
-                Start Working
-                </button>
-                <button
-                    className="short"
-                    onClick={this.setTimeForShortBreak}
-                >
-                Short Break
-                </button>
-                <button
-                    className="long"
-                    onClick={this.setTimeForLongBreak}
-                >
-                Long Break
-                </button>
-            </div>
+                <div className="timer">
+                    {this.displayTimer(time)}
+                </div>
+                <div className="types">
+                    <button
+                        className="start"
+                        onClick={this.setTimeForWork}
+                    >
+                    Start Working
+                    </button>
+                    <button
+                        className="short"
+                        onClick={this.setTimeForShortBreak}
+                    >
+                    Short Break
+                    </button>
+                    <button
+                        className="long"
+                        onClick={this.setTimeForLongBreak}
+                    >
+                    Long Break
+                    </button>
+                </div>
 
-        </div>
-      )
+                <div className="types">
+                    <button disabled={this.state.btnplayDisabled}
+                        className={this.state.btnplayDisabled ? 'reset' :'play'}
+                        onClick={() =>this.playTime(type)}
+                    >
+                    Play
+                    </button>
+                    <button
+                        className="pause"
+                        onClick={() => this.plauseTime(type)}
+                    >
+                    Pause
+                    </button>
+                    <button
+                        className="reset"
+                        onClick={() =>this.resetTime(type, message)}
+                    >
+                    Reset
+                    </button>
+                </div>
+
+            </div>
+        )
     }
 }
