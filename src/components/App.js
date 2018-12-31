ecea5e67259ff3529454eabb9d5d6b5abf23f5e0
werliton <werlitoncarlos@gmail.com>
2018-12-31 18:47:56 -0300

    Creating an accumulater of the numbers

--- src/components/App.js
+++ src/components/App.js
@@ -2,15 +2,17 @@ import React, { Component } from 'react';
 import './App.css';
 import Content from './Home/Content';
 import Footer from '../shared/component/layout/Footer'; 
-import Animation from './Animation/Animation'
+import Header from '../shared/component/layout/Header'
+import Numbers from './Numbers/Numbers'
 
 class App extends Component {
 
   render() {
     return (
       <div className="App">
+          <Header title="Entendendo Pure Components" />
           <Content>
-            <Animation />
+            <Numbers />
           </Content>
         <Footer />
       </div>
---
+++ src/components/Numbers/Numbers.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react'
+import Result from './Result'
+import './Numbers.css'
+
+export default class Numbers extends Component {
+    state = {
+        numbers:'', // Aqui serão salvos os valores de entrada
+        results:[] //Serão salvos os resultados
+    }
+
+    handleNumberChange = e => {
+        const {target: {value}} = e
+        // Convertendo o valor de string para array
+        // Ex: 123456 => [1,2,3,4,5,6]
+        const numbers = Array.from(value)
+        // Soma todos os valores do array
+        const results = numbers.reduce((a,b) => Number(a) + Number(b))
+        
+        this.setState({
+            numbers: value,
+            results: [...this.state.results, results]
+        })
+
+    }
+
+    render() {
+        return (
+        <div className="Numbers">
+            <input
+                type="number"
+                value={this.state.numbers}
+                onChange={this.handleNumberChange}
+            />
+            {/* Rendering the results array */}
+            <ul>
+                {this.state.results.map((result, i) => (
+                    <Result key={i} result={result} />
+                ))}
+            </ul>
+        </div>
+        )
+    }
+}
