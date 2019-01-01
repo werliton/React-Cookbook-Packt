9504c0e45b57960b8526e5ed708375e97a16979f
werliton <werlitoncarlos@gmail.com>
2019-01-01 16:54:56 -0300

    Creating menu in the header

--- src/components/App.js
+++ src/components/App.js
@@ -4,12 +4,14 @@ import Content from './Home/Content';
 import Footer from '../shared/component/layout/Footer'; 
 import Helmet from 'react-helmet'
 import PropTypes from 'prop-types'
+import Header from '../shared/component/layout/Header';
 
 const App = ({children}) =>(
 <div className="App">
       <Helmet 
         title="Routing"
         />
+      <Header />
       <Content>
           {children}
       </Content>
--- src/shared/component/layout/Header.js
+++ src/shared/component/layout/Header.js
@@ -1,15 +1,29 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import PropTypes from 'prop-types'
 import logo from '../../images/logo.svg'
 
-const Header = ({title, url}) => (
-    <header className="App-header">
-        <a href={url}>
-            <img src={logo} className="App-logo" alt="logo" />
-        </a>
-        <h1 className="App-title">{title}</h1>
-    </header>
-)
+const Header = props => {
+    const {
+        title = 'Usando rotas',
+        url = 'http://localhost:3000'
+    } = props
+
+    return(
+        <header className="App-header">
+            <a href={url}>
+                <img src={logo} className="App-logo" alt="logo" />
+            </a>
+            <h1 className="App-title">{title}</h1>
+            <ul>
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/about">About</Link></li>
+                <li><Link to="/notes">Notes</Link></li>
+                <li><Link to="/contact">Contact</Link></li>
+            </ul>
+        </header>
+    )
+}
 
 Header.propTypes = {
     title: PropTypes.string.isRequired,
