4c880391ecaf1dde1335647ca1c8a5f808ec34d5
werliton <werlitoncarlos@gmail.com>
2019-01-01 14:51:05 -0300

    Creating components to routes

---
+++ src/components/Error/404.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const Error404 = () => (
+    <div className="Error404">
+        <h1>Error404</h1>
+    </div>
+);
+export default Error404;
---
+++ src/components/Home/index.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+const Home = () => (
+    <div className="Home">
+        <h1>Home</h1>
+    </div>
+)
+
+export default Home
